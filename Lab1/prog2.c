a26c766ffd26b75ad9b6107f87bb1d9f8be6f463
NeolGut <neolgutierrez@gmail.com>
2023-01-25 13:01:28 -0500

    fine tuned prog2.c

--- Lab1/prog2.c
+++ Lab1/prog2.c
@@ -7,16 +7,18 @@ Lab 1 part 2
 #include <stdio.h>
 #include <math.h>
 
-// Pythagoream theorem
+// The equation being emulated is the pythagoream theorem
 
 int main(){
     float sideA, sideB, sideC;
-    printf("Please input side A: ");
+
+    printf("Please input leg A: ");
     scanf("%f",&sideA);
-    printf("Please input side B: ");
+    printf("Please input leg B: ");
     scanf("%f",&sideB);
 
-    sideC = sqrt(pow(sideA,2)+pow(sideB,2));
-    printf("The 3 side has a length of %f",sideC );
+    sideC = sqrt(pow(sideA,2)+pow(sideB,2)); // using the pow command to square each side, then adding them together and using the sqrt command
+    printf("The hyptoenus side has a length of %f",sideC );
+
     return 0;
 }
